bc742ecdc12f70773fb7432cf9f78f9df68a900c
jstesting20232023 <132028962+jstesting20232023@users.noreply.github.com>
2024-07-20 00:57:57 -0300

    Update tabata-timer.js

--- tabata-timer.js
+++ tabata-timer.js
@@ -29,7 +29,9 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                 clearInterval(timer);
                 document.getElementById('time').innerText = "00:00";
-                alarm.play(); // Reproducir el sonido de la alarma
+                alarm.play().catch(function(error) {
+                    console.log('Audio playback failed: ' + error);
+                });
             }
         }, 1000);
     }
